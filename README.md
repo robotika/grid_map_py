73840914e9b2d2da97821ab3711caa912ce65385
Martin Dlouhy <md@robotika.cz>
2025-07-14 10:06:54 +0200

    Initial attempt on Ubuntu Linux (ODroid)

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# grid_map_py
-ANYmal grid_map Python binding
+\# Grid Map Python Bindings
+
+
+
+This repository provides Python bindings for the powerful C++ grid map library from \[ANYbotics](https://github.com/anybotics/grid\_map).
+
+
+
+This allows for the creation, manipulation, and processing of 2D grid maps with multiple data layers directly in Python, making it useful for robotics applications, path planning, and terrain mapping.
+
+
+
+\## Original Library
+
+
+
+All credit for the core library goes to the team at ANYbotics.
+
+\* \*\*Original Repository:\*\* \[github.com/anybotics/grid\_map](https://github.com/anybotics/grid\_map)
+
+
+
+\## Installation
+
+
+
+```bash
+
+\# Clone this repository and its dependencies
+
+git clone --recurse-submodules \[https://github.com/](https://github.com/)<your-username>/grid\_map\_py
+
+cd grid\_map\_py
+
+
+
+\# Install build tools and build the wheel
+
+pip install build
+
+python -m build --wheel
+
+
+
+\# Install the built package
+
+pip install dist/grid\_map\_py-\*.whl
+
+
+
---
+++ setup.py
@@ -0,0 +1,43 @@
+import sys
+from setuptools import setup, Extension
+import pybind11
+
+# The definitive and complete list of C++ source files to compile.
+sources = [
+    'bindings.cpp',
+    '../grid_map/grid_map_core/src/GridMap.cpp',
+    '../grid_map/grid_map_core/src/GridMapMath.cpp',
+    '../grid_map/grid_map_core/src/BufferRegion.cpp',
+    '../grid_map/grid_map_core/src/SubmapGeometry.cpp',
+    '../grid_map/grid_map_core/src/Polygon.cpp',
+    '../grid_map/grid_map_core/src/BicubicInterpolation.cpp',
+    '../grid_map/grid_map_core/src/iterators/GridMapIterator.cpp',
+    '../grid_map/grid_map_core/src/iterators/SubmapIterator.cpp',
+    '../grid_map/grid_map_core/src/iterators/CircleIterator.cpp',
+    '../grid_map/grid_map_core/src/iterators/SpiralIterator.cpp',
+    '../grid_map/grid_map_core/src/iterators/LineIterator.cpp',
+    '../grid_map/grid_map_core/src/iterators/PolygonIterator.cpp'
+]
+
+ext_modules = [
+    Extension(
+        'grid_map_py',
+        sources,  # Use the complete list of sources
+        include_dirs=[
+            pybind11.get_include(),
+            '../grid_map/grid_map_core/include',
+            '../eigen'
+        ],
+        language='c++',
+        extra_compile_args=['-std=c++17', '-O3'],
+    ),
+]
+
+setup(
+    name='grid_map_py',
+    version='0.0.1',
+    author='Your Name',
+    author_email='your@email.com',
+    description='Python bindings for ANYbotics grid_map',
+    ext_modules=ext_modules,
+)
